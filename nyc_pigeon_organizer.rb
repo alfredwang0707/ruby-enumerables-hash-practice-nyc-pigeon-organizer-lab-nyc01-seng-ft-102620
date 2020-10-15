3b652a7a0b2157ff9a6add6515c16c755b0f44b4
Alfred Wang <alfredwang0707@gmail.com>
2020-10-15 21:45:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ require "pry"
 def nyc_pigeon_organizer(data)
   data.each_for_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
-  binding.pry 
-    end
+  names.each do |
+   end
 final_array
   end
 binding.pry
