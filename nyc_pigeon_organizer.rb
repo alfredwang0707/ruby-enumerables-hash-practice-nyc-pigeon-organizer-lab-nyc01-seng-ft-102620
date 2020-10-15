6633924a8a1407e1df10074576227cdd9bf96c41
Alfred Wang <alfredwang0707@gmail.com>
2020-10-15 21:51:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,8 @@ def nyc_pigeon_organizer(data)
     value.each do |inner_key, names|
   names.each do |name|
     binding.pry
-    if !final_array
+    if !final_array[name]
+      
   end
    end
 final_array
