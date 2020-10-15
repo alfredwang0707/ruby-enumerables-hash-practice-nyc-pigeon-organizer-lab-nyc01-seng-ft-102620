6b0543314f57905e639ed429ae3bf872fb8736b8
Alfred Wang <alfredwang0707@gmail.com>
2020-10-15 20:33:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,5 @@
 require "pry"
+#create a hash where pigeon names are the keys, with each name pointing to a hash #of their attributes.
 def nyc_pigeon_organizer(data)
   # write your code here!
 end
